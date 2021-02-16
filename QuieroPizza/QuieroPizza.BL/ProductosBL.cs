571e668a4c8f176ac966d2378e86a68302701e30
DELL <DELL@DESKTOP-PV5V4EA>
2021-02-16 11:24:22 -0600

    Agregando Entity Framework code First

--- QuieroPizza/QuieroPizza.BL/ProductosBL.cs
+++ QuieroPizza/QuieroPizza.BL/ProductosBL.cs
@@ -8,31 +8,22 @@ namespace QuieroPizza.BL
 {
     public class ProductosBL
     {
-       public  List<Producto> ObtenerProductos()
+        Contexto _contexto;
+
+        public List<Producto>listadeProductos { get; set; }
+
+            public ProductosBL()
         {
-
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = "Pizza 6 Quesos";
-            producto1.Precio = 200;
-
-            var producto2 = new Producto();
-            producto2.Id = 1;
-            producto2.Descripcion = "Pizza 4 Estaciones";
-            producto2.Precio = 250;
-
-            var producto3 = new Producto();
-            producto3.Id = 1;
-            producto3.Descripcion = "Pizza Jamon y Queso";
-            producto3.Precio = 180;
-
-            var ListadeProductos = new List<Producto>();
-            ListadeProductos.Add(producto1);
-            ListadeProductos.Add(producto2);
-            ListadeProductos.Add(producto3);
-            return ListadeProductos;
+            _contexto = new Contexto();
+            listadeProductos = new List<Producto>();
         }
 
-
+    
+       public  List<Producto> ObtenerProductos()
+        {
+            listadeProductos = _contexto.Productos.ToList();
+            return listadeProductos;
+        }
     }
+
 }
---
+++ QuieroPizza/QuieroPizza.BL/contexto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuieroPizza.BL
+{
+   public class Contexto : DbContext
+    {
+        public Contexto(): base(@"Data Source = (LocalDB)\MSSQLLocalDB;AttachDBFilename=" +
+            Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\QuieroPizzaDB.mdf")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+        }
+
+
+        public DbSet <Producto> Productos { get; set; }
+    }
+}
